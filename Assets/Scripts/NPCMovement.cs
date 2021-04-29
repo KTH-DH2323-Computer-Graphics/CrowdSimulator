2ad6e195d1c576ac5fab57e5e0cb4a978bff091a
Adam Jonsson <adamsmajl@gmail.com>
2021-04-29 21:11:26 +0200

    #23 feat: add walking rotation to npcs (#28)

---
+++ Assets/Scripts/NPCMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using AvoidanceLogic;
+using UnityEngine;
+
+public class NPCMovement : MonoBehaviour
+{
+    public Vector3 movementDirection = Vector3.forward;
+    public float speed = -2.0f;
+    
+    IAvoider avoiderComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.avoiderComponent = this.GetComponent<Avoider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 avoidanceVector = avoiderComponent.GetPreferredAvoidanceVector(
+            this.gameObject.transform.position, 
+            movementDirection
+        );
+        Vector3 finalMovement = movementDirection * speed + avoidanceVector;
+        Vector3 finalDirection = finalMovement.normalized;
+
+        transform.rotation = Quaternion.LookRotation(finalDirection);
+        transform.Translate(finalDirection * speed * Time.deltaTime, Space.World);
+    }
+
+}
