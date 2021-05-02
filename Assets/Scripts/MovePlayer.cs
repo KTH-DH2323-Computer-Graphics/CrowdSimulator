d3319924b737d2586b9a309549133188c2615796
Ella Klara Westerlund <40182097+ellaklara@users.noreply.github.com>
2021-05-02 21:56:48 +0200

    #37 feat: add gravity to player (#38)

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -6,16 +6,34 @@ public class MovePlayer : MonoBehaviour
 {
     public CharacterController controller;
     public float speed = 5f;
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
 
     // Update is called once per frame
     void Update()
     {
-       float x = Input.GetAxis("Horizontal");
+       isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
        
+       // To handle gravity when grounded
+       if(isGrounded && velocity.y < 0) {
+           velocity.y = -2f;
+       }
+       
+       float x = Input.GetAxis("Horizontal");
        float z = Input.GetAxis("Vertical");
        
-       Vector3 move = transform.right * x + transform.forward * z; //Player will move based on what direction they are facing
+       Vector3 move = transform.right * x + transform.forward * z; // Player will move based on what direction they are facing
        
        controller.Move(move * speed * Time.deltaTime); 
+
+       // To apply gravity to the player
+       velocity.y += gravity * Time.deltaTime;
+       controller.Move(velocity * Time.deltaTime);
     }
 }
